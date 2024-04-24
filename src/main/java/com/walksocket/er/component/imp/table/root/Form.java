4c4dafd28c7054d8af934533359d385bf78ae8bf
Shigenobu Furuta <shigenobu@walksocket.com>
2024-04-24 17:06:05 +0900

    Version 0.6.1.
    * Modify html.
    * Modify several import.

--- src/main/java/com/walksocket/er/component/imp/table/root/Form.java
+++ src/main/java/com/walksocket/er/component/imp/table/root/Form.java
@@ -3,9 +3,12 @@ package com.walksocket.er.component.imp.table.root;
 import com.walksocket.er.Size.DialogSmall;
 import com.walksocket.er.Utils;
 import java.awt.Dimension;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.text.JTextComponent;
 
 /**
  * Form.
@@ -27,6 +30,12 @@ public class Form extends JPanel {
    */
   public Form() {
     textAreaDdl.setText(expressionStarting);
+    textAreaDdl.addFocusListener(new FocusAdapter() {
+      @Override
+      public void focusGained(FocusEvent e) {
+        ((JTextComponent) e.getComponent()).selectAll();
+      }
+    });
     var sp = new JScrollPane(textAreaDdl);
     sp.setPreferredSize(new Dimension(DialogSmall.WIDTH - 40, DialogSmall.HEIGHT / 10 * 8));
     sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
